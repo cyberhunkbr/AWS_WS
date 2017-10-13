07494128d139f4f2988e2a691bca297a750c44c9
Bala Gongale <babasaheb.gongale@atos.net>
2017-10-13 17:25:54 +0530

    Removing credentials

--- HelloEC2/src/main/java/com/amazonaws/helloec2/Hello_EC2.java
+++ HelloEC2/src/main/java/com/amazonaws/helloec2/Hello_EC2.java
@@ -1,15 +1,26 @@
 package com.amazonaws.helloec2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.client.apache.config.*;
-
-import java.net.URI;
-
-import org.apache.commons.httpclient.protocol.Protocol;
-
+import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.DescribeVolumesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.Volume;
 /**
  * @author a572393
  *
@@ -47,12 +58,10 @@ public class Hello_EC2 {
 	 public static String retrieveMetadata(Type instanceId, int timeout,String defaultValue) {
 		 
 		 try{
-		 URI uri = URI.create("http://ec2-54-191-222-236.us-west-2.compute.amazonaws.com/latest/meta-data/" + instanceId);
+		 URI uri = URI.create("http://54.245.29.19/latest/meta-data/instance-id");
 		 System.out.println(uri.toString());
 		 ClientConfig config = new DefaultApacheHttpClientConfig();
-		// config.setProtocol(Protocol.HTTP);
-		// config.setProxyHost("proxy.com");
-		 //config.setProxyPort(1234);
+
 
 		 config.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT,timeout);
 		 Client client = Client.create(config);
@@ -69,11 +78,62 @@ public class Hello_EC2 {
 		 
 	 }
 	 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Hello_EC2 ec2 = new Hello_EC2();
-		 String myEC2Id = retrieveMetadata(Type.instance_id,5000, null);
-	        System.out.println("The Instance Id is " + myEC2Id + " .");
+	public static String retrieveInstanceId() throws Exception {
+		String EC2Id = "";
+		String inputLine;
+		URL EC2MetaData = new URL("http://54.245.29.19/latest/meta-data/instance-id");
+		URLConnection EC2MD = EC2MetaData.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				EC2MD.getInputStream()));
+		while ((inputLine = in.readLine()) != null) {
+			EC2Id = inputLine;
+		}
+		in.close();
+		System.out.println("EC2Id="+EC2Id);
+		return EC2Id;
 	}
 
-}
+	 
+	public static void main(String[] args) throws Exception {
+		
+		AWSCredentials credentials = new BasicAWSCredentials("<Access key ID>", "<Secret Key Id>");
+	    @SuppressWarnings("deprecation")
+		AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+	    ec2.setEndpoint("ec2.us-west-2.amazonaws.com");
+	    AmazonEC2 ec3 = new AmazonEC2Client(credentials);
+	    
+	 
+	    DescribeInstancesResult insResult = ec2.describeInstances();
+	 
+	    int count = 1;
+	    for (Reservation reservation : insResult.getReservations())
+	    {
+	      for(Instance instance : reservation.getInstances())
+	      {
+	        System.out.println("Instance # " + count++
+	                + "\n InstanceId: " + instance.getInstanceId()
+	                + "\n InstanceType: " + instance.getInstanceType()
+	                + "\n Public IP: " + instance.getPublicIpAddress());
+	        StopInstancesRequest request = new StopInstancesRequest().withInstanceIds( instance.getInstanceId());
+	        ec2.stopInstances(request);
+
+	      }
+	    }
+	    
+	    DescribeVolumesResult volReq = ec2.describeVolumes();
+	    
+	    int countt = 1;
+	    for (Volume vol : volReq.getVolumes())
+	    {
+	      System.out.println("Volume " + count   + "\n Details: " + vol);
+	      count++;
+	    }
+		// TODO Auto-generated method stub
+		Hello_EC2 ec = new Hello_EC2();
+		//ec.retrieveInstanceId();
+		
+		// String myEC2Id = retrieveMetadata(Type.instance_id,5000, null);
+	    // System.out.println("The Instance Id is =" + myEC2Id + " .");
+	}
+
+}	
